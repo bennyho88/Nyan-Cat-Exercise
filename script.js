4a8fcdc57d8e4248ebe7ce138a72e62ef72e7460
Benny Ho <benny.ho2530@gmail.com>
2019-05-29 11:22:02 +0200

    added function that will hide the button

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function myMove() {
   document.getElementById("cat").addEventListener("click", myMove);
 
   function myMove() {
+    document.getElementById("cat").style.display = "none";
     var backgroundI = document.body.style.backgroundImage = "url('background.png')";
     var audio = new Audio("Nyan.mp3");
     audio.play();
@@ -35,13 +36,14 @@ function myMove() {
     var elem = document.getElementById("nyanimg");
     var pos = 0;
     var id = setInterval(frame, 10);
-    
+   
     function frame() {
       if (pos >= 100) {
         clearInterval(id);
 
         audio.pause();
         document.getElementById("background-image").style.backgroundImage = "none";
+        document.getElementById("cat").style.display = "block";
      
       } else {
         pos+= 0.1;
