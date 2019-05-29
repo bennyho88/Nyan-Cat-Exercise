f5a07654c2f077515ca840af05352386be658672
Benny Ho <benny.ho2530@gmail.com>
2019-05-29 10:23:55 +0200

    fixed problems with background image and audio

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-
-
-/* document.querySelector("#cat").addEventListener("click", nyanpicture);
-
-function nyanpicture() {
-    document.querySelector("#nyanimg")
-}
-
-*/ 
+/*
 
 document.getElementById("cat").addEventListener("click", myMove );
 
 function myMove() {
-    
+    document.body.style.backgroundImage = "url('background.png')";
     var audio = new Audio("Nyan.mp3")
     audio.play();
     
@@ -30,6 +22,35 @@ function myMove() {
       }
     }
 
-    document.getElementsByClass("background-image").style.backgroundImage = "url(background.png)";
+  }
+*/
+
+  document.getElementById("cat").addEventListener("click", myMove);
+
+  function myMove() {
+    var backgroundI = document.body.style.backgroundImage = "url('background.png')";
+    var audio = new Audio("Nyan.mp3");
+    audio.play();
+
+    var elem = document.getElementById("nyanimg");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    
+    function frame() {
+      if (pos >= 100) {
+        clearInterval(id);
+
+        audio.pause();
+        document.getElementById("background-image").style.backgroundImage = "none";
+     
+      } else {
+        pos+= 0.1;
+        elem.style.top = pos + "%";
+        elem.style.left = pos + "%";
+       
+      }
+    }
+    
+    
   }
 
